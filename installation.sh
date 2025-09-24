c75d8cdc7fdf156a96a1e18daa8b32c8a16a248f
Chshivani860 <shivanichokka522gmail.com>
2025-09-24 10:32:49 +0530

    file added

--- installation.sh
+++ installation.sh
@@ -4,12 +4,14 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
     echo "Error:: Please run this script with root privelege"
+    exit 1
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0]; then 
+if [ $? -ne 0 ]; then 
     echo "Error:: Installing mysql is failed"
+    exit 1
 else
     echo "Installation is SUCCESS"
 
