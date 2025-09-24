4d291b33d464cfc3ceedf753c0dfdbc57138a3d1
Chshivani860 <shivanichokka522gmail.com>
2025-09-24 11:50:57 +0530

    file added

--- installation.sh
+++ installation.sh
@@ -2,6 +2,11 @@
 
 userid=$(id -u)
 
+R=/e[31m
+G=/e[32m
+Y=/e[33m
+N=/e[0m
+
 if [ $userid -ne 0 ]; then
     echo "Error:: Please run this script with root privelege"
     exit 1
@@ -10,19 +15,34 @@ fi
 validate(){
 
     if [ $1 -ne 0 ]; then 
-        echo "Error:: Installing $2 is failed"
+        echo  -e "Installing $2............. $R FAILURE $N"
         exit 1
     else
-        echo "Installing $2 is SUCCESS"
+        echo -e "Installing $2.............. $G SUCCESS $N"
 
     fi
 }
-dnf install mysql -y
-validate $? "mysql"
 
-dnf install nginx -y
-validate $? "nginx"
+dnf list install mysql
+if [ $? -ne 0 ]; then
+
+    dnf install mysql -y
+    validate $? "mysql"
+else
+    echo -e "mysql is already exist ............ $G skipping $N"
 
 
-dnf install python3 -y
-validate $? "python3"
+dnf list install nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    validate $? "nginx"
+else    echo -e "nginx is already exist............. $G skipping  $N"
+
+
+
+dnnf list install python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    validate $? "python3"
+else
+    echo -e "python3 already exist............ $G skipping $N"
