353fa440700d69f9a8542d9c76cdd216a11108a9
Chshivani860 <shivanichokka522gmail.com>
2025-09-24 12:41:15 +0530

    file added

--- logs.sh
+++ logs.sh
@@ -1,11 +1,18 @@
-
-
 userid=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+
+logs_floders="/var/lod/shell-script"
+script_name=$(echo $0 | cut -d "." -f1)
+log_files="$logs_folders/$script_name.log"
+
+
+mkdir -p $logs_folder
+echo "script started excuted at: $(date)"
 
 
 if [ $userid -ne 0 ]; then
@@ -24,26 +31,26 @@ validate(){
     fi
 }
 
-dnf list install mysql
+dnf list install mysql &>>$log_file
 if [ $? -ne 0 ]; then
 
-    dnf install mysql -y
+    dnf install mysql -y&>>$log_file
     validate $? "mysql"
 else
     echo -e "mysql is already exist ............ $Y skipping $N"
 fi
 
-dnf list install nginx
+dnf list install nginx &>>$log_file
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>>$log_file
     validate $? "nginx"
 else    echo -e "nginx is already exist............. $Y skipping  $N"
 fi
 
 
-dnf list install python3
+dnf list install python3 &>>$log_file
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y &>>$log_file
     validate $? "python3"
 else
     echo -e "python3 already exist............ $Y skipping $N"
