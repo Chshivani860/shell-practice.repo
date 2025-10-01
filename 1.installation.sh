94f615ae7b05c81d3b0f3db2228ee564653344f5
Chshivani860 <shivanichokka522gmail.com>
2025-10-01 19:06:09 +0530

    file updated

--- 1.installation.sh
+++ 1.installation.sh
@@ -4,5 +4,13 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then 
     echo "Error:: Please run this script with root previlage"
-    #exit 1
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "Error:: Mysql installation is FAILED"
+else
+    echo "Mysql installation is SUCCESS"
 fi
