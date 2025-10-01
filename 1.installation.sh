ee0649659020fb99cb573b6fecd1c767c716a24a
Chshivani860 <shivanichokka522gmail.com>
2025-10-01 21:05:00 +0530

    file updated

--- 1.installation.sh
+++ 1.installation.sh
@@ -7,6 +7,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
+logs_folders="/var/log/shell-script"
+script_name=$( echo $0 | cut -d "." -f1 )
+log_files="$logs_floders/$script_name.log"
+
+mkdir -p $logs_folder
+echo " script  started executed at: $(date) "
+
 if [ $userid -ne 0 ]; then 
     echo -e "$R Eror:: Please run this script with root previlage $N"
     exit 1
@@ -20,27 +28,27 @@ validate(){
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>log_files
 
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf install mysql -y &>>log_files
     validate $? "mysql"
 else
     echo -e " mysql is already exist ....$Y SKIPPING $N "
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>log_files
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>>log_files
     validate $? "nginx"
 else
     echo -e " nginx already exist ....$Y SKIPPING $N "
 fi
 
-dnf list installed python3
+dnf list installed python3 &>>log_files
 
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y &>>log_files
     validate $? "python3"
 else
     echo -e "python3 already exist ....$Y SKIPPING $N "
