521250c1cee935bfc62473a036106855cb5379e6
Chshivani860 <shivanichokka522gmail.com>
2025-10-01 19:51:56 +0530

    file updated

--- 1.installation.sh
+++ 1.installation.sh
@@ -14,3 +14,19 @@ if [ $? -ne 0 ]; then
 else
     echo "Mysql installation is SUCCESS"
 fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Error :: nginx installation FAILED"
+else 
+    echo "Nginx installation is SUCCESS"
+fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ]; then 
+    echo "Error :: python3 installation is FAILED"
+else 
+    echo "python3 installation is SUCCESS"
+fi
