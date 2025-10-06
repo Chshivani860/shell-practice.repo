9f32daf7d1c44029235e1554f71dfe664bd82833
Chshivani860 <shivanichokka522gmail.com>
2025-10-06 11:21:02 +0530

    file exist

--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@ Body=$4
 Ip_address=$5
 To_team=$6
 
-final_body=$(sed -e "s/To_team/$To_team/g" -e "s/alert_type/$alert_type/g" -e "s/Ip_address/$Ip_address/g" -e "s/Body/$Body/g")
+final_body=$(sed -e "s/To_team/$To_team/g" -e "s/alert_type/$alert_type/g" -e "s/Ip_address/$Ip_address/g" -e "s/Body/$Body/g" templet.html)
 
 
 
